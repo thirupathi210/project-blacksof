16a2773cdb521b086d375d4968316f25d28061f0
Thirupathi <jthirupathi210@gmail.com>
2025-03-22 12:22:33 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './Component/first';
+function App() {
+  return (
+    <div className="App">
+      <First/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/first.js
@@ -0,0 +1,453 @@
+import React from "react";
+
+const First = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="../Images/logo.png" alt="Logo" />
+          </a>
+
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            class="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
+            <div class="d-flex align-items-center">
+              <a href="#contact-form" class="btn btn-primary me-2">
+                Contact Us
+              </a>
+              {/* <a href="#" class="btn btn-secondary me-3">Button 2</a> */}
+              <a href="#" class="me-2">
+                <i class="bi bi-linkedin social"></i>
+              </a>
+              <a href="#">
+                <i class="bi bi-facebook social"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      {/* second section */}
+
+      <div class="second-section">
+        <div class="text-center second-title text-white">
+          <p>Performance in motion</p>
+          <h2>
+            Soft Trims and NVH Solutions <br /> <span>for seamless rides</span>
+          </h2>
+        </div>
+      </div>
+
+      {/* third section */}
+
+      <div
+        id="carouselExampleAutoplaying"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+          <div class="slide-secton">
+        <div class="header-text">
+          Evolving the drive with <span>360-degree</span> nonwoven solutions
+        </div>
+
+        <div class="container">
+          <div class="row content-section">
+            <div class="col-md-4 left-text">
+              <h3>Passenger vehicles</h3>
+              <p>Revving up Nonwoven innovation from interior to exterior..</p>
+              {/* <h6 class="mt-4">Commercial vehicles</h6>
+              <p>Adding lightweight engineering to heavy-duty vehicles.</p> */}
+            </div>
+
+            <div class="col-md-8 car-image">
+              <img src="../images/1.png" alt="360-degree car model" />
+              {/* <p class="click-hint">Click anywhere for next slide<br/>[Only written for prototype]</p> */}
+            </div>
+          </div>
+        </div>
+
+        <div class="bottom-nav">
+          <img src="../Images/c1.png" alt="Complete Body" />
+          <img src="../Images/c2.png" alt="Front" />
+          <img src="../Images/c3.png" alt="Cabin" />
+          <img src="../Images/c4.png" alt="Trunk" />
+          <img src="../Images/c5.png" alt="Exterior" />
+        </div>
+      </div>
+            {/* <img src="../Images/Frame.png" class="d-block w-100" alt="..."/> */}
+          </div>
+          <div class="carousel-item ">
+          <div class="slide-secton">
+        <div class="header-text">
+          Evolving the drive with <span>360-degree</span> nonwoven solutions
+        </div>
+
+        <div class="container">
+          <div class="row content-section">
+            <div class="col-md-4 left-text">
+              <h3>Commercial vehicles</h3>
+              <p>Advancing Nonwoven engineering for heavy-duty vehicles.</p>
+             
+            </div>
+
+            <div class="col-md-8 car-image">
+              <img src="../images/2.png" alt="360-degree car model" />
+              {/* <p class="click-hint">Click anywhere for next slide<br/>[Only written for prototype]</p> */}
+            </div>
+          </div>
+        </div>
+
+        <div class="bottom-nav">
+          <img src="../Images/c1.png" alt="Complete Body" />
+          <img src="../Images/c2.png" alt="Front" />
+          <img src="../Images/c3.png" alt="Cabin" />
+          <img src="../Images/c4.png" alt="Trunk" />
+          <img src="../Images/c5.png" alt="Exterior" />
+        </div>
+      </div>
+          </div>
+          <div class="carousel-item">
+          <div class="slide-secton">
+        <div class="header-text">
+          Evolving the drive with <span>360-degree</span> nonwoven solutions
+        </div>
+
+        <div class="container">
+          <div class="row content-section">
+            <div class="col-md-4 left-text">
+              <h3>Passenger vehicles</h3>
+              <p>Driving up Nonwoven innovation from interior to exterior.</p>
+              {/* <h6 class="mt-4">Commercial vehicles</h6>
+              <p>Adding lightweight engineering to heavy-duty vehicles.</p> */}
+            </div>
+
+            <div class="col-md-8 car-image">
+              <img src="../images/3.png" alt="360-degree car model" />
+              {/* <p class="click-hint">Click anywhere for next slide<br/>[Only written for prototype]</p> */}
+            </div>
+          </div>
+        </div>
+
+        <div class="bottom-nav">
+          <img src="../Images/c1.png" alt="Complete Body" />
+          <img src="../Images/c2.png" alt="Front" />
+          <img src="../Images/c3.png" alt="Cabin" />
+          <img src="../Images/c4.png" alt="Trunk" />
+          <img src="../Images/c5.png" alt="Exterior" />
+        </div>
+      </div>
+          </div>
+          <div class="carousel-item">
+          <div class="slide-secton">
+        <div class="header-text">
+          Evolving the drive with <span>360-degree</span> nonwoven solutions
+        </div>
+
+        <div class="container">
+          <div class="row content-section">
+            <div class="col-md-4 left-text">
+              <h3>Passenger vehicles</h3>
+              <p>Revving up Nonwoven innovation from interior to exterior.</p>
+              {/* <h6 class="mt-4">Commercial vehicles</h6>
+              <p>Adding lightweight engineering to heavy-duty vehicles.</p> */}
+            </div>
+
+            <div class="col-md-8 car-image">
+              <img src="../images/4.png" alt="360-degree car model" />
+              {/* <p class="click-hint">Click anywhere for next slide<br/>[Only written for prototype]</p> */}
+            </div>
+          </div>
+        </div>
+
+        <div class="bottom-nav">
+          <img src="../Images/c1.png" alt="Complete Body" />
+          <img src="../Images/c2.png" alt="Front" />
+          <img src="../Images/c3.png" alt="Cabin" />
+          <img src="../Images/c4.png" alt="Trunk" />
+          <img src="../Images/c5.png" alt="Exterior" />
+        </div>
+      </div>
+          </div>
+          <div class="carousel-item">
+          <div class="slide-secton">
+        <div class="header-text">
+          Evolving the drive with <span>360-degree</span> nonwoven solutions
+        </div>
+
+        <div class="container">
+          <div class="row content-section">
+            <div class="col-md-4 left-text">
+              <h3>Passenger vehicles</h3>
+              <p>Revving up Nonwoven innovation from interior to exterior.</p>
+              {/* <h6 class="mt-4">Commercial vehicles</h6>
+              <p>Adding lightweight engineering to heavy-duty vehicles.</p> */}
+            </div>
+
+            <div class="col-md-8 car-image">
+              <img src="../images/5.png" alt="360-degree car model" />
+              {/* <p class="click-hint">Click anywhere for next slide<br/>[Only written for prototype]</p> */}
+            </div>
+          </div>
+        </div>
+
+        <div class="bottom-nav">
+          <img src="../Images/c1.png" alt="Complete Body" />
+          <img src="../Images/c2.png" alt="Front" />
+          <img src="../Images/c3.png" alt="Cabin" />
+          <img src="../Images/c4.png" alt="Trunk" />
+          <img src="../Images/c5.png" alt="Exterior" />
+        </div>
+      </div>
+          </div>
+          <div class="carousel-item">
+          <div class="slide-secton">
+        <div class="header-text">
+          Evolving the drive with <span>360-degree</span> nonwoven solutions
+        </div>
+
+        <div class="container">
+          <div class="row content-section">
+            <div class="col-md-4 left-text">
+              <h3>Passenger vehicles</h3>
+              <p>Driving up Nonwoven innovation from interior to exterior.</p>
+              <h6 class="mt-4">Commercial vehicles</h6>
+              <p>Adding lightweight engineering to heavy-duty vehicles.</p>
+            </div>
+
+            <div class="col-md-8 car-image">
+              <img src="../images/5.png" alt="360-degree car model" />
+              {/* <p class="click-hint">Click anywhere for next slide<br/>[Only written for prototype]</p> */}
+            </div>
+          </div>
+        </div>
+
+        <div class="bottom-nav">
+          <img src="../Images/c1.png" alt="Complete Body" />
+          <img src="../Images/c2.png" alt="Front" />
+          <img src="../Images/c3.png" alt="Cabin" />
+          <img src="../Images/c4.png" alt="Trunk" />
+          <img src="../Images/c5.png" alt="Exterior" />
+        </div>
+      </div>
+          </div>
+          <div class="carousel-item">
+          <div class="slide-secton">
+        <div class="header-text">
+          Evolving the drive with <span>360-degree</span> nonwoven solutions
+        </div>
+
+        <div class="container">
+          <div class="row content-section">
+            <div class="col-md-4 left-text">
+              <h3>Passenger vehicles</h3>
+              <p>Revving up Nonwoven innovation from interior to exterior.</p>
+              {/* <h6 class="mt-4">Commercial vehicles</h6>
+              <p>Adding lightweight engineering to heavy-duty vehicles.</p> */}
+            </div>
+
+            <div class="col-md-8 car-image">
+              <img src="../images/1.png" alt="360-degree car model" />
+              {/* <p class="click-hint">Click anywhere for next slide<br/>[Only written for prototype]</p> */}
+            </div>
+          </div>
+        </div>
+
+        <div class="bottom-nav">
+          <img src="../Images/c1.png" alt="Complete Body" />
+          <img src="../Images/c2.png" alt="Front" />
+          <img src="../Images/c3.png" alt="Cabin" />
+          <img src="../Images/c4.png" alt="Trunk" />
+          <img src="../Images/c5.png" alt="Exterior" />
+        </div>
+      </div>
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      {/* slide section */}
+
+      
+      {/* fourth section */}
+
+      <div id="contact-form" class="contact-container form">
+        <div class="row contact-box">
+          <div class="col-md-5">
+            <h2>Get in touch</h2>
+            <hr class="border-white" style={{ width: "50px;" }} />
+            <p>For general enquiries</p>
+            <p>
+              <strong>Address:</strong> 110, 16th Road, Chembur, Mumbai - 400071
+            </p>
+            <p>
+              <strong>Phone:</strong> +91 22 25203822
+            </p>
+            <p>
+              <strong>Email:</strong> info@supremegroup.co.in
+            </p>
+          </div>
+
+          <div class="col-md-7">
+            <form>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Full name"
+                  required
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="email"
+                  class="form-control"
+                  placeholder="E-mail"
+                  required
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Subject"
+                  required
+                />
+              </div>
+              <div class="mb-3">
+                <textarea
+                  class="form-control"
+                  rows="3"
+                  placeholder="Message"
+                  required
+                ></textarea>
+              </div>
+              <button type="submit" class="btn btn-send">
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      {/* footer section */}
+
+      <footer class="footer">
+        <div class="container">
+          <div class="row text-center text-md-start">
+            <div class="col-md-3 d-flex flex-column align-items-center align-items-md-start">
+              <div class="footer-logo">
+                <img src="../Images/logo.png" alt="Supreme Group" />
+              </div>
+            </div>
+
+            <div class="col-md-2">
+              <h6 class="fw-bold">APPLICATIONS</h6>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">Apparel</a>
+                </li>
+                <li>
+                  <a href="#">Automotive</a>
+                </li>
+                <li>
+                  <a href="#">Filtration</a>
+                </li>
+                <li>
+                  <a href="#">Customized Solutions</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-md-2">
+              <h6 class="fw-bold">COMPANY</h6>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">Innovation</a>
+                </li>
+                <li>
+                  <a href="#">Global Competency</a>
+                </li>
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-md-2">
+              <h6 class="fw-bold">MORE</h6>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">Careers</a>
+                </li>
+                <li>
+                  <a href="#">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="#">Terms and Conditions</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-md-3">
+              <h6 class="fw-bold">FOLLOW US</h6>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">Twitter</a>
+                </li>
+                <li>
+                  <a href="#">LinkedIn</a>
+                </li>
+                <li>
+                  <a href="#">Instagram</a>
+                </li>
+                <li>
+                  <a href="#">Medium</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="text-center footer-bottom">
+            <p>©2023. All Rights Reserved.</p>
+            <p>Supreme House, 110, 16th Road Chembur, Mumbai - 400071</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default First;
